eb7506fc3345c9b1b5a8fba3714bc2b64c37f800
juan <juandavid20006@hotmail.com>
2018-02-20 01:11:18 +0000

    fix to homepage

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use((req, res, next)=> {
     
 // });
 
-app.get('/home',(req, res)=>{
+app.get('/',(req, res)=>{
     res.render('home.hbs',{
         pagetitle: 'Welcome Home',
         welcomeMessage: 'You are very welcome',
